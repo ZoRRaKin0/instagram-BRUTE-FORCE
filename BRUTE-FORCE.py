6b15d528be718d74bd1ba47644e261c68bb95b5a
ZoRRaKin <105127547+ZoRRaKin0@users.noreply.github.com>
2022-06-24 23:00:14 +0300

    Add files via upload

---
+++ BRUTE-FORCE.py
@@ -0,0 +1,215 @@
+#coded by ZoRRaKiN
+proxy = """
+İP ADRESLERİ    PORT   ÜLKE
+167.71.5.83	3128	NL	
+47.91.44.217	8000	AU	
+8.210.83.33	80	HK	
+80.48.119.28	8080	PL	
+66.29.154.103	3128	US	
+149.19.224.31	3128	US	
+103.232.215.194	80	SG	
+169.57.1.85	8123	MX	
+185.61.152.137	8080	GB	
+103.219.194.13	80	US	
+185.76.10.68	8081	NL	
+47.254.39.233	80	US	
+47.74.18.113	80	JP	
+103.148.72.126	80	HK	
+103.219.194.12	80	US	
+103.219.194.11	80	US	
+67.212.186.99	80	US	
+43.250.107.91	80	HK	
+8.209.210.211	80	JP	
+103.149.162.195	80	HK	
+49.207.36.81	80	IN	
+159.203.61.169	3128	CA	
+103.148.72.192	80	HK	
+121.1.41.162	111	PH	
+195.158.24.46	3128	UZ	
+194.233.69.90	443	SG	
+157.230.48.102	80	US	
+95.59.26.129	80	KZ	
+43.255.113.232	8082	KH	
+115.96.208.124	8080	IN	
+64.227.62.123	80	US	
+103.159.46.2	83	IN	
+45.248.41.216	9812	IN	
+175.111.129.156	8080	IN	
+196.2.15.68	8080	BI	
+178.252.175.16	8080	IR	
+191.102.64.147	999	CO	
+66.94.97.238	443	US	
+93.240.114.68	4003	DE	
+103.124.137.40	8080	ID	
+20.47.108.204	8888	US	
+200.116.198.222	9812	CO	
+175.100.64.127	9812	KH	
+187.217.54.84	80	MX	
+213.230.97.10	3128	UZ	
+213.6.199.94	49044	PS	
+136.228.243.159	8082	JP	
+164.155.147.1	80	US	
+164.155.145.1	80	US	
+103.132.52.178	8080	ID	
+129.226.17.43	80	IN	
+114.7.27.98	8080	ID	
+159.89.195.14	80	SG	
+112.78.132.97	8080	ID	
+115.241.197.126	80	IN	
+164.155.149.1	80	US	
+173.255.210.187	80	US	
+38.55.182.225	80	US	
+45.250.66.104	9812	IN	
+194.233.69.126	443	SG	
+34.87.84.105	80	SG	
+208.67.183.240	80	US	
+38.55.181.255	80	US	
+38.55.177.225	80	US	
+35.233.16.212	80	BE	
+205.201.49.132	53281	US	
+183.111.25.253	8080	KR	
+181.176.211.168	8080	PE	
+114.4.104.254	3128	ID	
+157.100.53.100	999	EC	
+59.124.224.205	3128	TW	
+165.90.225.15	3389	ZA	
+94.181.48.110	1256	RU	
+45.195.76.130	999	DO	
+112.137.142.8	3128	VN	
+91.195.156.111	41890	UA	
+157.100.12.138	999	EC	
+182.253.107.212	8080	ID	
+85.208.209.214	8085	US	
+213.166.79.98	8085	US	
+154.201.44.56	3128	FR	
+154.201.45.181	3128	FR	
+77.83.87.217	8085	US	
+91.188.246.216	8085	US	
+45.67.212.102	8085	RU	
+213.166.78.223	8085	US	
+88.218.66.148	8085	RU	
+95.181.151.114	8085	RU	
+45.155.68.246	8250	SC	
+154.201.41.103	3128	FR	
+95.181.148.63	8085	RU	
+83.171.254.181	8085	RU	
+37.44.254.161	8085	RU	
+85.239.59.62	8085	US	
+193.233.82.245	8085	US	
+193.233.142.130	8085	US	
+172.252.231.156	3128	FR	
+213.166.79.60	8085	US	
+45.67.212.19	8085	RU	
+91.242.228.104	8085	US	
+138.128.97.132	7722	US	
+213.166.79.89	8085	US	
+109.236.52.210	8085	US	
+85.209.151.11	8085	US	
+192.241.116.82	8636	US	
+85.208.86.136	8085	RU	
+47.245.58.113	80	JP	
+95.181.149.139	8085	RU	
+142.252.198.158	3128	FR	
+104.227.100.249	8330	US	
+194.110.150.219	8085	US	
+85.239.56.249	8085	US	
+167.172.157.96	80	US	
+142.252.223.136	3128	FR	
+38.55.180.225	80	US	
+164.155.150.0	80	US	
+164.155.148.0	80	US	
+51.195.144.68	8888	FR	
+146.59.199.12	80	FR	
+82.119.150.122	8080	RU	
+103.245.204.214	8080	BD	
+203.142.71.50	8080	ID	
+36.91.98.115	8181	ID	
+95.216.96.39	3128	FI	
+164.155.150.1	80	US	
+164.155.151.0	80	US	
+164.155.146.0	80	US	
+91.244.66.174	80	RU	
+85.195.104.71	80	DE	
+117.54.114.97	80	ID	
+47.74.0.7	80	JP	
+194.233.73.107	443	SG	
+103.130.141.98	8080	KH	
+88.199.164.141	8081	PL	
+185.118.129.43	8080	IQ	
+103.105.212.106	53281	PH	
+119.42.152.252	8080	IN	
+103.69.38.1	8080	IN	
+188.40.148.168	8080	DE	
+95.0.219.240	8080	TR	
+45.70.15.2	8080	EC	
+190.94.211.207	999	VE	
+46.105.35.193	8080	FR	
+51.15.42.134	8118	NL	
+103.60.160.88	8080	BD	
+203.177.133.235	8080	PH	
+14.241.39.191	8080	VN	
+190.109.205.253	999	HN	
+200.125.171.202	9991	DO	
+197.243.20.186	80	RW	
+103.219.193.200	80	US	
+149.54.11.76	80	AF	
+45.5.68.25	999	PE	
+179.184.224.91	3128	BR	
+185.82.96.162	9093	LB	
+147.135.38.83	24333	US
+162.144.116.103	80	US	
+45.32.101.24	80	SG	
+137.184.197.190	80	US
+"""
+import time
+import os
+import random
+import webbrowser
+os.system('pkg install python')
+os.system ('python -m pip install colorama')
+kullanici_adi = str(input("kullanıcı adı giriniz :"))
+kullanici_id = str(input("kullanıcı id giriniz :"))
+onay = str(input("saldırı baslayacak onaylıyormusunuz evet/hayır :"))
+time.sleep(3)
+os.system('clear')
+sifre = str(input("SİFREYİ GİRİNİZ :"))
+if sifre =="ZoRRaKin1905" or sifre == "ZoRRaKina1b2c3d4e5":
+    if onay =="evet":
+        print("SALDIRI İÇİN ONAY ALINDI ATTACK BAŞLATILIYOR...")
+        time.sleep(6)
+        webbrowser.open_new('https://www.instagram.com/')
+        time.sleep(1.2)
+        attack_kaydi = os.system('netstat -an -p -tcp')
+        for denenen_sifre in range(1,8359,1):
+            time.sleep(0.3)
+            print(denenen_sifre,"sifre denendi...")
+        print("ATTACK BİTTİ... SONUCLAR GELİYOR LÜTFEN BEKLEYİNİZ...")
+        time.sleep(10)
+        os.system('clear')
+        telegram=str(input("TELEGRAM İSMİNİZİ BASINDA @ OLCAK SEKILDE YAZINIZ :"))   
+        print("SONUÇ BASARILI.",telegram,"İSİMLİ TELEGRAM ADRESİNİZE SONUÇLAR GÖNDERİLİYOR...")
+        time.sleep(5)
+        print ("TELEGRAM BOTU BAKIMDA OLDUGU İCİN BİLGİLER GELEMİYOR...")
+        time.sleep(5)
+        os.system('clear')
+        proxys=str(input("KULLANILAN PROXY'LERİ GORMEK ISTERMİSİNİZ evet/hayır :"))
+        if proxys == "evet":
+            print(proxy)
+            print("ATTACK İCİN TOPLAM 160 PROXY KULLANILDI.")
+        else:
+            time.sleep(2.1)
+            os.system('clear')
+        attack_gecmisi_sorgu = str(input("SİZE YAPILAN ATTACK GEÇMİŞİNİ GÖRMEK İSTERMİSİNİZ  evet/hayır :"))
+        if attack_gecmisi_sorgu == "evet":
+            print("attack gecmisiniz görüntüleniyor",attack_kaydi,"attack yapılmıstır.")
+        else:
+            os.system('clear')
+            time.sleep(1.2)
+            print("ATTACK SONLANDIRILDI. İYİ GÜNLER:)")
+         
+    else:
+        print ("SALDIRI İÇİN ONAY ALINAMADI ATTACK BAŞLATILMIYOR...")
+        time.sleep(5)
+        os.system('clear')
+else:
+    print("sifre yanlıs lutfen tekrar deneyın")
